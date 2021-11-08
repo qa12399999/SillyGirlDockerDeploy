04502f4cc0211da53020769ea87643241029f4d9
qa12399999 <85656851+qa12399999@users.noreply.github.com>
2021-11-08 17:19:39 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -83,7 +83,7 @@ import (
     _ "github.com/cdle/sillyGirl/develop/qinglong"
     _ "github.com/cdle/sillyGirl/develop/jd_cookie"
     _ "github.com/cdle/sillyGirl/im/qq"
-    _ "github.com/cdle/sillyGirl/im/wxmp"
+    _ "github.com/cdle/sillyGirl/im/wx"
     _ "github.com/cdle/sillyGirl/im/tg"
 )
 EOF
